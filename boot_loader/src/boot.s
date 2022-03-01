c2b47a44ac44c6f90e29f7f0996cefebc0f6059d
hardfau18 <the.qu1rky.b1t@gmail.com>
2022-03-01 18:59:32 +0530

    writing to screen working

--- boot_loader/src/boot.s
+++ boot_loader/src/boot.s
@@ -6,12 +6,23 @@ entry:
     // setup stack with stack address later given in linker.ld script
     ldr sp, =stack_top
     // no need of BL since it will never return
-    B loop
+    ldr r1, =console
+    ldr r1, [r1]        // get the uart value
+    ldr r2, =msg
+print_msg:
+    ldrB r3, [r2], #1       // get character in r2 and increment r2
+    strB r3, [r1]
+    cmp r3, #0              // if the character is 0 (null) then loop infinitely
+    BNE print_msg
+
 // loop infinitely
 loop:
     B loop
 
+msg: .string "Booting your OS"
+console:.word 0x09000000
+
 .org 0x1FE
 // bootsector flag
-.word 0xaa55
+.short 0xaa55
 
